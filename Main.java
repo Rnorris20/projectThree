61a72f026b77e969f3f1e73b2256f090d3b69110
RNorris6 <ryan.norris@patelhs.org>
2020-11-06 17:43:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+  
+  int [] seatNumbers = {1,2,4,7,12,13,14,15,16,18,19,20,22,23,25};
+
+  String[] {?}
+
+  for(int 1 = 15; i>= 0; i--)
+  }
+}
