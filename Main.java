e29573a8f0dd8c910a2fc159c6207ed290b52028
RNorris6 <ryan.norris@patelhs.org>
2020-11-13 23:52:56 +0000

    I added a scanner to ask the users imput about their names

--- Main.java
+++ Main.java
@@ -1,11 +1,111 @@
+import java.util.Scanner;
+
 class Main {
+
   public static void main(String[] args) {
   
-  int [] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  int  seatNumbers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-  String [] studentNames = {Ryan , John , Mark , George , Lucy , Lauren , Bob , James , Bryan , Jaden , Jake , Nancy , Mia , Emma , Olivia};
+  String students[] = {"Ryan" , "John" , "Mark" , "George" , "Lucy" , "Lauren" , "Bob" , "James" , "Bryan" , "Jaden" , "Jake" , "Nancy" , "Mia" , "Emma" , "Olivia" };
 
-  for(int 1 = 15; i>= 0; i--);
+for (int i = 0; i < students.length; i++)
+     {
+         System.out.println(students[i] );
+     }
 
-  }
+
+
+        Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 1st student, What is your name?");
+
+        scan.next();
+// this is where the first scanner will end and the second will begin
+
+        Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 2nd student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 3rd student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 4th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 5th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 6th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 7th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 8th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 9th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 10th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 11th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 12th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 13th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 14th student, What is your name?");
+          
+        scan.next();
+
+Scanner scan = new Scanner(System.in);
+
+          System.out.println("Hello 15th student, What is your name?");
+          
+        scan.next();
+
+
+  } 
 }
